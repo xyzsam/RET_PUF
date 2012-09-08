aff71ec3e0bebdb2b650546419fd45317788747f
Sam Xi <slxi1202@gmail.com>
2012-09-08 00:57:28 -0400

    fixed sign issues with percent difference.

--- +analysis/twohistanalyze.m
+++ +analysis/twohistanalyze.m
@@ -6,9 +6,9 @@
 % same PUF is used, for example.
 %
 % SYNTAX:
-%   function stats = twohistanalyze(encrypt_struct, decrypt_struct, mode)
-%     encrypt_struct: structure containing encryption data
-%     decrypt_struct: structure containing decryption data
+%   function stats = twohistanalyze(data_1, data_2, mode)
+%     data_1: A structure containing PUF data.
+%     data_2: A structure containing PUF data.
 %     mode: A string indicating which statistics and metrics to compute.
 %       Valid modes:
 %         'counts_diff': Computes the difference in total counts and the maximum
@@ -27,27 +27,40 @@
 %       The fifth element is the maximum difference between the two histograms
 %         at any one given time with respect to the first.
 %       The sixth element is the maximum percent difference between the two
-%         histograms at any one given time with respect to the first.
+%         histograms at any one given time with respect to the maximum count at
+%         at any one given time of the first histogram.
 %     To prevent strange data collection errors from intefering with
 %     this metric, all leading and trailing zeros have been stripped from the
 %     histograms.
 %
 % Author: Sam Xi
-function result = twohistanalyze(encrypt_struct, decrypt_struct, mode)
+function stats = twohistanalyze(data_1, data_2, mode)
   if (strcmp(mode, 'counts_diff'))
-    if (length(encrypt_struct.graph) > length(decrypt_struct.graph))
-      end_index = length(decrypt_struct.graph);
-    else
-      end_index = length(encrypt_struct.graph);
-    end
-    graph_1 = encrypt_struct.graph(1:end_index);
-    graph_2 = decrypt_struct.graph(1:end_index);
-    encrypt_total_counts = sum(graph_1);
-    decrypt_total_counts = sum(graph_2);
+    end_index = @getShorterLength(data_1.graph, data_2.graph);
+    graph_1 = data_1.graph(1:end_index);
+    graph_2 = data_2.graph(1:end_index);
+    d1_total_counts = sum(graph_1);
+    d2_total_counts = sum(graph_2);
     max_diff = max(abs(graph_1 - graph_2));
-    total_count_diff = encrypt_total_counts - decrypt_total_counts;
-    result = [encrypt_total_counts decrypt_total_counts ...
-              total_count_diff total_count_diff/encrypt_total_counts*100 ...
-              max_diff max_diff/encrypt_total_counts*100];
+    max_graph_1 = max(graph_1);
+    total_count_diff = d1_total_counts - d2_total_counts;
+    stats = [d1_total_counts d2_total_counts ...
+              total_count_diff total_count_diff/d1_total_counts*100 ...
+              max_diff max_diff/max_graph_1*100];
+  elseif (strcmp(mode, 'xcorr_hist'))
+    cross_corr = xcorr(data_1.graph, data_2.graph);
+    auto_corr = xcorr(data_1.graph, data_1.graph);
+    corr_integral = trapz(corr);
+    corr_max = max(corr);
+    corr_ratio = corr_max/max(auto_corr);
+    stats = [corr_integral corr_max corr_ratio];
+  end
+end
+
+function short_length = getShorterLength(hist1, hist2)
+  if (length(hist1) > length(hist2))
+    short_length = length(hist2)
+  else
+    short_length = length(hist1)
   end
 end

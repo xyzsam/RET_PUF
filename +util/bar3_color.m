0fddb68a097c8d6a00941a3e68a8108e5917064b
Sam Xi <slxi1202@gmail.com>
2012-09-17 01:11:00 -0400

    added function to plot bar chart with appropriate color shading.

---
+++ +util/bar3_color.m
@@ -0,0 +1,26 @@
+% BAR3_COLOR    Plots a 3D bar chart with an amplitude colormap.
+%
+% By default, bar3 colors the bar chart by column of data. This plots the bar
+% chart and shades it by comparative amplitude, so that trends are more easily
+% seen. The colormap used is 'jet'.
+%
+% SYNTAX:
+%     handle = bar3_color(data) plots data as a 3D barchart, colors it by
+%       comparative amplitude, and returns a handle to the figure.
+%
+% Obtained from: http://www.mathworks.com/matlabcentral/newsreader/view_thread/
+% 141194
+
+function handle = bar3_color(data)
+  barmin = min(data(:));
+  barmax = max(data(:));
+  bh=bar3(data);
+  for i=1:length(bh)
+    set(bh(i),'cdata',...
+    get(bh(i),'zdata'));
+  end
+  colormap(jet(barmax+1));
+  caxis([barmin barmax]);
+  colorbar;
+  shading interp;
+end

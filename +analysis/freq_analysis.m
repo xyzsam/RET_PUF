19bdb680bbb47932fd158b2d1c95df8e820d75e3
Sam Xi <slxi1202@gmail.com>
2012-10-19 12:56:32 -0400

    script to perform frequency analysis on an array of floating point values.

---
+++ +analysis/freq_analysis.m
@@ -0,0 +1,25 @@
+% FREQ_ANALYSIS   Performs alphabetic frequency analysis on an array of doubles.
+%
+% freqs = freq_analysis(m) converts the floating point values in array m into
+% IEEE754 format, parses them into 8 bit chunks, and builds a histogram of the
+% number of occurrences of each 8 bit segment. This histogram is returned as
+% freqs, where freqs(i) is the number of occurrences of the 8 bit
+% representation of the integer i-1 (due to MATLAB array indexing).
+%
+% Author: Sam Xi
+
+function freqs = freq_analysis(m)
+  if (~isfloat(m) || size(m, 1) > 1)
+    error('m must be an single dimension array of floating point values.');
+  end
+
+  freqs = zeros(1, 256);
+  for i=1:length(m)
+    s = util.ieee754(m(i));
+    s8bit = [s(1:8); s(9:16); s(17:24); s(25:32); ...
+             s(33:40); s(41: 48); s(49: 56); s(57:64)];
+    intvalues = bin2dec(s8bit);
+    intvalues = uint8(intvalues);  % Convert from double to int.
+    freqs(intvalues+1) = freqs(intvalues+1) + 1;
+  end
+end

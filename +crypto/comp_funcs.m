c26231411c5d575f4c1b2fbe4c59b25e29030d7a
Sam Xi <slxi1202@gmail.com>
2013-02-23 16:41:53 -0500

    l2norm to ssd
    l2norm to ssd label change.

--- +crypto/comp_funcs.m
+++ +crypto/comp_funcs.m
@@ -9,7 +9,8 @@
 % Syntax:
 %   func = comp_funcs(type)
 %   type: A string. Valid types are:
-%     'l2norm': Sum of squares of differences.
+%     'l2norm_hist': Sum of squares of differences of the histograms.
+%     'l2norm_loghist': Sum of squares of differences of ln(histograms).
 %     'conv_max': Maximum of the convolution.
 %     'conv_int': Integral of the convolution, with the trapezoidal approximation.
 %     'rel_conv_max': Compares the maximum of the convolution of the two data
@@ -21,8 +22,10 @@
 %     a similarity value. Syntax: val = func(data1, data2);
 
 function func = comp_funcs(type)
-  if (strcmp(type, 'l2norm'))
+  if (strcmp(type, 'l2norm_hist'))
     func = @sum_square;
+  elseif (strcmp(type, 'l2norm_loghist'))
+    func = @sum_square_log;
   elseif (strcmp(type, 'conv_max'))
     func = @conv_max;
   elseif (strcmp(type, 'conv_int'))
@@ -43,6 +46,19 @@ end
     val = 1/val;  % This makes larger values correspond to greater similarity.
   end
   
+  % Computes the sum of squares of differences on the log of the raw counts,
+  % which have been normalized to an area of 1.
+  function val = sum_square_log(data1, data2)
+    data1 = log(data1);
+    data2 = log(data2);
+    longer_length = max(length(data1), length(data2));
+    combined = zeros(2, longer_length);
+    combined(1, 1:length(data1)) = data1/trapz(data1);
+    combined(2, 1:length(data2)) = data2/trapz(data2);
+    l2n = sqrt(sum((combined(1,:)-combined(2,:)).^2));
+    val = 1/l2n;  % This makes larger values correspond to greater similarity.
+  end
+
   function val = conv_int(data1, data2)
     c = conv(data1, data2);
     val = trapz(c);
